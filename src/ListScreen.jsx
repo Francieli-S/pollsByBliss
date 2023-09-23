d4feb49a5429cc4cf115cc05780019f6527ce9b4
Francieli Sauthier <francieli.sauthier@gmail>
2023-09-23 11:50:47 +0200

    get list all questions

--- src/ListScreen.jsx
+++ src/ListScreen.jsx
@@ -1,5 +1,46 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 export default function ListScreen() {
+  const [questionsList, setQuestionsList] = useState([]);
+
+  let limit = questionsList.length;
+  let offset = limit;
+  let filter = '';
+
+  const axiosQuestions = async () => {
+    try {
+      const response = await axios.get(
+        `https://private-anon-205e1ac56d-blissrecruitmentapi.apiary-mock.com/questions?limit=${limit}&offset=${offset}&filter=${filter}`
+      );
+      if (response.status === 200) {
+        setQuestionsList(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    axiosQuestions();
+  }, []);
+
   return (
-    <div>ListScreen</div>
-  )
+    <div>
+      {questionsList.map((question) => (
+        <div key={question.id}>
+          <h3>
+            {question.id} {question.question}
+          </h3>
+          <ul>
+            {question.choices.map((choice, i) => (
+              <li key={choice[i]}>
+                Language: {choice.choice} Votes: {choice.votes}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
